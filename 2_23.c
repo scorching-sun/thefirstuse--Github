732ed38ca42ac8a3b3223b11db654d10e4aca848
scorching-sun <2951936681@qq.com>
2022-03-03 20:03:53 +0800

    .

---
+++ 2_23.c
@@ -0,0 +1,20 @@
+//#include <stdio.h>
+////2022/2/23 19点21分  
+////功能：使用变量互换两个变量的值
+//void swap(int* p, int* q)
+//{
+//	int temp;
+//	temp = *p;
+//	*p = *q;
+//	*q = temp;
+//}
+//
+//int main()
+//{
+//int a = 3, * p, c = 5, * q;
+//p = &a; //把变量a的地址赋值给指针p，即p指向a
+//q= &c;
+//swap(p, q);
+//printf("a = %d,c = %d \n", a, c);
+//return 0;
+//}
---
+++ 3_2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{  
+	/*int a = 3;
+	a += a -= a * a;
+	printf("%d\n", a);
+	int b = 2;
+	int c = 2;
+	int d = 2;
+	int w = b++ + c++ + d++;
+	printf("%d\n", w);
+	d = a-- - a; printf("%d\n", d);*/
+	/*double x = 1.0, y;
+	y = x + 3/2;
+	printf("%f\n", y);*/
+	int a = 1, b = 0;
+	int w = !a && !b;
+	printf("%d", w);
+
+}
